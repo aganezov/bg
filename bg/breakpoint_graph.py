3dc8d702c023f962674f6762573dedbe621d795f
sergey-aganezov-jr <sergeyaganezovjr@gmail.com>
2015-01-26 13:26:48 -0500

    implemented expansion of add_edge method implementing merging tactic from multicolors perspective. Equipped with a suited test case

--- bg/breakpoint_graph.py
+++ bg/breakpoint_graph.py
@@ -9,8 +9,12 @@ class BreakpointGraph(object):
     def __init__(self):
         self.bg = MultiGraph()
 
-    def add_edge(self, vertex1, vertex2, multicolor, *args, **kwargs):
+    def add_edge(self, vertex1, vertex2, multicolor, *args, merge=True, **kwargs):
         attr_dict = {"multicolor": multicolor}
         attr_dict.update(kwargs)
+        if self.bg.has_edge(u=vertex1, v=vertex2):
+            if merge:
+                self.bg[vertex1][vertex2][0]["multicolor"] += multicolor
+                return
         self.bg.add_edge(u=vertex1, v=vertex2,
                          attr_dict=attr_dict)
