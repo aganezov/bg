31b9cd1c06bcefd0bbc94f1f54600d734b073f4c
aganezov <aganezov@gwu.edu>
2015-01-27 14:50:45 -0500

    implemented a test suite for a case, when two edges are attempted to be merged, but those edges connect different vertices

--- tests/test_edge.py
+++ tests/test_edge.py
@@ -38,6 +38,30 @@ class BGEdgeTestCase(unittest.TestCase):
         self.assertEqual(merged_edge.vertex2, v2)
         self.assertEqual(merged_edge.multicolor, multicolor + multicolor1)
 
+    def test_merging_incorrect(self):
+        v1 = BGVertex("v1")
+        v2 = BGVertex("v2")
+        v3 = BGVertex("v3")
+        v4 = BGVertex("v4")
+        multicolor = Multicolor("blue")
+        multicolor1 = Multicolor("green")
+        edge1 = BGEdge(vertex1=v1, vertex2=v2, multicolor=multicolor)
+        edge2 = BGEdge(vertex1=v1, vertex2=v3, multicolor=multicolor1)
+        with self.assertRaises(ValueError):
+            BGEdge.merge(edge1, edge2)
+        edge2 = BGEdge(vertex1=v3, vertex2=v2, multicolor=multicolor1)
+        with self.assertRaises(ValueError):
+            BGEdge.merge(edge1, edge2)
+        edge2 = BGEdge(vertex1=v3, vertex2=v4, multicolor=multicolor1)
+        with self.assertRaises(ValueError):
+            BGEdge.merge(edge1, edge2)
+        edge2 = BGEdge(vertex1=v1, vertex2=v1, multicolor=multicolor1)
+        with self.assertRaises(ValueError):
+            BGEdge.merge(edge1, edge2)
+        edge2 = BGEdge(vertex1=v2, vertex2=v2, multicolor=multicolor1)
+        with self.assertRaises(ValueError):
+            BGEdge.merge(edge1, edge2)
+
 
 if __name__ == '__main__':
     unittest.main()
