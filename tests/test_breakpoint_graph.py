261c61a7cd53277b69d1bc22e97ac0849ba088fa
aganezov <aganezov@gwu.edu>
2015-01-27 18:56:29 -0500

    initial take on edge_splitting method and suitable test suit. Not passing test suite as of yet, needed additional implementation of edge deletion inside edge_splitting

--- tests/test_breakpoint_graph.py
+++ tests/test_breakpoint_graph.py
@@ -62,5 +62,34 @@ class BreakpointGraphTestCase(unittest.TestCase):
         self.assertEqual(graph.bg.edges(v1, data=True)[0][2]["multicolor"], multicolor)
         self.assertEqual(graph.bg.edges(v1, data=True)[1][2]["multicolor"], multicolor2)
 
+    def test_correct_edge_splitting_no_guidance(self):
+        graph = BreakpointGraph()
+        v1 = BGVertex("v1")
+        v2 = BGVertex("v2")
+        multicolor = Multicolor("black")
+        multicolor1 = Multicolor("green")
+        multicolor2 = Multicolor("blue")
+        multicolor3 = Multicolor("yellow")
+        graph.add_edge(vertex1=v1, vertex2=v2, multicolor=multicolor)
+        graph.add_edge(vertex1=v1, vertex2=v2, multicolor=multicolor1)
+        graph.add_edge(vertex1=v1, vertex2=v2, multicolor=multicolor2)
+        graph.add_edge(vertex1=v1, vertex2=v2, multicolor=multicolor3)
+        self.assertEqual(len(graph.bg.edges()), 1)
+        graph.split_edge(vertex1=v1, vertex2=v2, multicolor=multicolor + multicolor1 + multicolor2 + multicolor3)
+        self.assertEqual(len(graph.bg.edges(v1)), 4)
+        self.assertEqual(len(graph.bg.edges(v2)), 4)
+
+        graph = BreakpointGraph()
+        v1 = BGVertex("v1")
+        v2 = BGVertex("v2")
+        multicolor = Multicolor("black")
+        graph.add_edge(vertex1=v1, vertex2=v2, multicolor=multicolor)
+        self.assertEqual(len(graph.bg.edges(v1)), v1)
+        self.assertEqual(len(graph.bg.edges(v1)), v2)
+        graph.split_edge(vertex1=v1, vertex2=v2, multicolor=multicolor)
+        self.assertEqual(len(graph.bg.edges(v1)), v1)
+        self.assertEqual(len(graph.bg.edges(v1)), v2)
+
+
 if __name__ == '__main__':
     unittest.main()
