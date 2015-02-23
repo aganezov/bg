84ed7a6320e618c484e9bacfc268234386e3f2d4
sergey-aganezov-jr <sergeyaganezovjr@gmail.com>
2015-02-22 20:55:10 -0500

    providing simpler and shorter access to important classes in bg package

--- bg/__init__.py
+++ bg/__init__.py
@@ -4,3 +4,13 @@ __email__ = "aganezov(at)gwu.edu"
 __status__ = "develop"
 
 version = "0.9.0.dev2"
+
+__all__ = ["breakpoint_graph",
+           "vertex",
+           "multicolor",
+           "edge"]
+
+from bg.breakpoint_graph import BreakpointGraph
+from bg.vertex import BGVertex
+from bg.edge import BGEdge
+from bg.multicolor import Multicolor
