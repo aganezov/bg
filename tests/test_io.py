32764935f256258cb75f84e0ce9f27052f27527a
sergey-aganezov-jr <sergeyaganezovjr@gmail.com>
2015-03-01 11:15:05 -0500

    initial take on the GRIMM reader and io module in general

---
+++ tests/test_io.py
@@ -0,0 +1,13 @@
+__author__ = 'Sergey Aganezov'
+__email__ = "aganezov(at)gwu.edu"
+__status__ = "develop"
+
+import unittest
+
+
+class GRIMMReaderTestCase(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
