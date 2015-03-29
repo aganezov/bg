dabba51ee904951e689454f343287a84a72cb8e4
sergey-aganezov-jr <sergeyaganezovjr@gmail.com>
2015-03-29 19:40:01 -0400

    forget at first and then updated status of module file to develop as changes have been made to it

--- tests/test_vertex.py
+++ tests/test_vertex.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = "Sergey Aganezov"
 __email__ = "aganezov(at)gwu.edu"
-__status__ = "production"
+__status__ = "develop"
 
 import unittest
 from bg.vertex import BGVertex
