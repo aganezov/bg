40c7afe4c8a9a207ef7183c40d5089c4bc19f059
sergey-aganezov-jr <sergeyaganezovjr@gmail.com>
2015-04-23 00:32:06 -0400

    moved genome test case comments up to date

--- tests/test_genome.py
+++ tests/test_genome.py
@@ -8,18 +8,22 @@ from bg.genome import BGGenome, BGGenome_JSON_SCHEMA_JSON_KEY
 
 class BGGenomeTestCase(unittest.TestCase):
     def test_initialization_incorrect(self):
+        # empty genomes are not allowed, a name for genome is mandatory
         with self.assertRaises(TypeError):
             g = BGGenome()
 
     def test_initialization(self):
+        # simple correct initialization
         g = BGGenome("name")
         self.assertEqual(g.name, "name")
 
     def test_hash(self):
+        # hash of genome instance is proxies to hash value of its name
         g = BGGenome("name")
         self.assertEqual(hash(g), hash("name"))
 
     def test_json_id(self):
+        # json id for genome is utilized when genome is serialized to json format and equals to hash value of genome instance
         g = BGGenome("name")
         json_id = g.json_id
         self.assertEqual(json_id, hash(g.name))
@@ -31,6 +35,7 @@ class BGGenomeTestCase(unittest.TestCase):
         self.assertNotEqual(json_id, new_json_id)
 
     def test__eq__(self):
+        # two genome are called equal if they are both os same class and their hash values are equal
         g1 = BGGenome("name1")
         g2 = BGGenome("name2")
         self.assertNotEqual(g1, g2)
@@ -41,6 +46,7 @@ class BGGenomeTestCase(unittest.TestCase):
         self.assertNotEqual(g1, [g1])
 
     def test_json_serialization_no_subclassing(self):
+        # genome can be serialized into json format keeping all important information
         g = BGGenome("name1")
         ref_result = {
             "name": "name1",
@@ -68,6 +74,7 @@ class BGGenomeTestCase(unittest.TestCase):
             BGGenome.from_json(data={})
 
     def test_json_deserialization_subclassing(self):
+        # being provided an explicit JSONSchema, it shall be utilized for json deserialization
         class BGGenomeJSONSchemaNameOptional(BGGenome.BGGenomeJSONSchema):
             def make_object(self, data):
                 if "name" not in data:
@@ -76,6 +83,8 @@ class BGGenomeTestCase(unittest.TestCase):
         self.assertEqual(BGGenome.from_json(data={}, json_schema_class=BGGenomeJSONSchemaNameOptional).name, "default_name")
 
     def test__lt__(self):
+        # genome is less than any non BGGenome instance
+        # with other BGGenome instance it is compared by respective "name" attributes
         g1 = BGGenome("genome1")
         g2 = BGGenome("genome2")
         self.assertLess(g1, g2)
@@ -91,6 +100,8 @@ class BGGenomeTestCase(unittest.TestCase):
             self.assertLess(g2, object_to_compare_to)
 
     def test__le__(self):
+        # Genome is considered less or equal to any other BGGenome is it is either less ("<" implementation
+        # or equal (__eq__ implementation), than supplied argument
         g1 = BGGenome("genome1")
         g2 = BGGenome("genome1")
         self.assertLessEqual(g1, g2)
