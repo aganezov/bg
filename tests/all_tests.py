43fa6771a948eee52d13e127bae6e02effcf04c8
aganezov <aganezov@gwu.edu>
2015-10-31 09:46:09 -0400

    test

--- tests/all_tests.py
+++ tests/all_tests.py
@@ -10,6 +10,7 @@ import unittest
 from teamcity import is_running_under_teamcity
 from teamcity.unittestpy import TeamcityTestRunner
 
+
 if __name__ == '__main__':
     all_tests = unittest.TestLoader().discover('./', pattern='*.py')
     if is_running_under_teamcity():
