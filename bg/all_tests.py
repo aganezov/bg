77efd6f1e4eee4ac3c5815d5df38f491e25551db
aganezov <aganezov@gwu.edu>
2015-10-30 22:30:56 -0400

    teamcity testing setup

---
+++ bg/all_tests.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import glob
+import os
+
+__author__ = "aganezov"
+__email__ = "aganezov(at)gwu.edu"
+__status__ = "development"
+
+import unittest
+from teamcity import is_running_under_teamcity
+from teamcity.unittestpy import TeamcityTestRunner
+
+if __name__ == '__main__':
+    all_tests = unittest.TestLoader().discover('tests', pattern='*.py')
+    if is_running_under_teamcity():
+        runner = TeamcityTestRunner()
+    else:
+        runner = unittest.TextTestRunner()
+    runner.run(all_tests)
