80ea6a6b2d95c5aa0ae82c81a48e9ccf9748638d
aganezov <aganezov@gwu.edu>
2015-10-30 22:37:00 -0400

    teamcity setup correction... AGAIN!

--- tests/all_tests.py
+++ tests/all_tests.py
@@ -11,7 +11,7 @@ from teamcity import is_running_under_teamcity
 from teamcity.unittestpy import TeamcityTestRunner
 
 if __name__ == '__main__':
-    all_tests = unittest.TestLoader().discover('tests', pattern='*.py')
+    all_tests = unittest.TestLoader().discover('./', pattern='*.py')
     if is_running_under_teamcity():
         runner = TeamcityTestRunner()
     else:
