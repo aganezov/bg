6f144e52cac31b355d2cb2da7036482d8bdc6404
aganezov <aganezov@gwu.edu>
2015-12-30 12:22:22 -0500

    Recursive dict update. A work around the limitation of ChainMap being only one level deep

--- bg/utils.py
+++ bg/utils.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import collections
 
 
 def dicts_are_equal(dict1, dict2):
@@ -11,3 +12,13 @@ def dicts_are_equal(dict1, dict2):
         else:
             result &= dict1[key] == dict2[key]
     return result
+
+
+def recursive_dict_update(dict1, dict2):
+    for key, value in dict2.iteritems():
+        if isinstance(value, collections.Mapping):
+            r = recursive_dict_update(dict1.get(key, {}), value)
+            dict1[key] = r
+        else:
+            dict1[key] = dict2[key]
+    return dict1
