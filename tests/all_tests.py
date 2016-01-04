d75beb4a171193fc391a8a5e9c2a71d28da7c6fe
aganezov <aganezov@gwu.edu>
2016-01-03 22:53:56 -0500

    Teamcity setup expansion

--- tests/all_tests.py
+++ tests/all_tests.py
@@ -9,11 +9,15 @@ __status__ = "development"
 import unittest
 from teamcity import is_running_under_teamcity
 from teamcity.unittestpy import TeamcityTestRunner
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from bg import version
 
 if __name__ == '__main__':
     all_tests = unittest.TestLoader().discover('./', pattern='test_*.py')
     if is_running_under_teamcity():
         runner = TeamcityTestRunner()
+        print("###teamcity[setParameter name='env.FullVersion' value='{version}']".format(version=version))
     else:
         runner = unittest.TextTestRunner()
     runner.run(all_tests)
