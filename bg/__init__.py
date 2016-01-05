aa29b1a6e691068ad0e967c28029d126f19abf50
aganezov <aganezov@gwu.edu>
2016-01-05 12:02:00 -0500

    Version 1.5.2

--- bg/__init__.py
+++ bg/__init__.py
@@ -3,7 +3,7 @@ __author__ = "Sergey Aganezov"
 __email__ = "aganezov(at)gwu.edu"
 __status__ = "production"
 
-version = "1.5.1"
+version = "1.5.2"
 
 __all__ = ["bg_io",
            "breakpoint_graph",
