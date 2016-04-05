f9ad0e3877ad3528368ccd4642db92f88f370f3e
aganezov <aganezov@gwu.edu>
2016-04-05 12:14:41 -0400

    import optimization

--- tests/all_tests.py
+++ tests/all_tests.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
-import glob
 import os
+import sys
+import unittest
+
+from teamcity import is_running_under_teamcity
+from teamcity.unittestpy import TeamcityTestRunner
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from bg import version
 
 __author__ = "aganezov"
 __email__ = "aganezov(at)gwu.edu"
 __status__ = "development"
-
-import unittest
-from teamcity import is_running_under_teamcity
-from teamcity.unittestpy import TeamcityTestRunner
-import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-from bg import version
 
 if __name__ == '__main__':
     all_tests = unittest.TestLoader().discover('./', pattern='test_*.py')
