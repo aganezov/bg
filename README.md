43170bcd9a8c02799124d04086d1eb9240e92a99
aganezov <aganezov@gwu.edu>
2016-11-20 19:54:07 -0500

    Licence update to MIT + classifiers update in the setup.py script [ci skip]

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Breakpoint Graph
 [![Build Status](https://travis-ci.org/aganezov/bg.svg?branch=master)](https://travis-ci.org/aganezov/bg) 
 [![Coverage Status](https://coveralls.io/repos/aganezov/bg/badge.svg?branch=master)](https://coveralls.io/r/aganezov/bg?branch=master)
 
+[![PyPI version](https://badge.fury.io/py/bg.svg)](https://badge.fury.io/py/bg)
 [![Supported Python versions](https://pypip.in/py_versions/bg/badge.svg?style=flat)](https://pypi.python.org/pypi/bg/) 
 [![Wheel Status](https://pypip.in/wheel/bg/badge.svg?style=flat)](https://pypi.python.org/pypi/bg/)
 [![License](https://pypip.in/license/bg/badge.svg?style=flat)](https://pypi.python.org/pypi/bg/)
--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 __author__ = 'aganezov'
 import os
+
 from setuptools import setup
 
 from bg import version as bg_version
+
 setup(
     name="bg",
     version=bg_version,
@@ -11,15 +13,17 @@ setup(
     author="Sergey Aganezov",
     author_email="aganezov@gwu.edu",
     description="Implementation of Breakpoint Graph data structure",
-    license="LGPLv3",
+    license="MIT",
     keywords=["breakpoint graph", "data structures", "python"],
     url="https://github.com/aganezov/bg",
     classifiers=[
-        "Development Status :: 4 - Beta",
-        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Development Status :: 5 - Release",
+        "License :: OSI Approved :: MIT License",
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ]
 )
