0bf5d9f955c70fac156f90ad16f25274db8b3417
aganezov <aganezov@cs.jhu.edu>
2018-09-07 12:36:57 -0400

    commented out all teamcity related code lines

--- tests/all_tests.py
+++ tests/all_tests.py
@@ -1,23 +1,23 @@
-# -*- coding: utf-8 -*-
-import os
-import sys
-import unittest
-
-from teamcity import is_running_under_teamcity
-from teamcity.unittestpy import TeamcityTestRunner
-
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-from bg import version
-
-__author__ = "aganezov"
-__email__ = "aganezov(at)gwu.edu"
-__status__ = "development"
-
-if __name__ == '__main__':
-    all_tests = unittest.TestLoader().discover('./', pattern='test_*.py')
-    if is_running_under_teamcity():
-        runner = TeamcityTestRunner()
-        print("###teamcity[setParameter name='env.FullVersion' value='{version}']".format(version=version))
-    else:
-        runner = unittest.TextTestRunner()
-    runner.run(all_tests)
+# # -*- coding: utf-8 -*-
+# import os
+# import sys
+# import unittest
+#
+# from teamcity import is_running_under_teamcity
+# from teamcity.unittestpy import TeamcityTestRunner
+#
+# sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+# from bg import version
+#
+# __author__ = "aganezov"
+# __email__ = "aganezov(at)cs.jhu.edu"
+# __status__ = "development"
+#
+# if __name__ == '__main__':
+#     all_tests = unittest.TestLoader().discover('./', pattern='test_*.py')
+#     if is_running_under_teamcity():
+#         runner = TeamcityTestRunner()
+#         print("###teamcity[setParameter name='env.FullVersion' value='{version}']".format(version=version))
+#     else:
+#         runner = unittest.TextTestRunner()
+#     runner.run(all_tests)
